5c45451de1b9e82fccb53cd039e9686abe990d2f
izaro.rubio <izaro.rubio@euneiz.com>
2025-01-09 16:33:02 +0100

    next.config.ts change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    // Asegúrate de que la ruta al alias se resuelva correctamente
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+    return config;
+  },
 };
 
 export default nextConfig;
+
